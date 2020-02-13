a0f09f6419aed36c350a51f2f3b3806ccdb1ae01
tsai <mengjietai@163.com>
2020-02-13 14:55:00 +0800

    使用bootstrap 对blog_detail blog_list blog_with_type 页面进行了美化

--- MyBlogSite/blog/views.py
+++ MyBlogSite/blog/views.py
@@ -6,21 +6,27 @@ from .models import Blog, BlogType
 # 获取所有的博客进行显示
 def blog_list(request):
     context = {}
+    # 获取所有的博客
     context['blogs'] = Blog.objects.all()
-    return render(request, 'blog/blog_list.html', context)
+    # 获取所有的博客类型
+    context['blog_types'] = BlogType.objects.all()
+    return render(request, 'blog_list.html', context)
 
 
 # 根据博客的id获取对应博客数据，并且渲染出来
 def blog_detail(request, blog_pk):
     context = {}
     context['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    return render(request, 'blog/blog_detail.html', context)
+    return render(request, 'blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_type_pk):
     context = {}
-    print(blog_type_pk)
+    # 获取id=blog_type_pk的博客类型
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    # 当前博客类型
     context['blog_type'] = blog_type
-    return render(request, 'blog/blog_with_type.html', context)
+    # 获取所有的博客类型
+    context['blog_types'] = BlogType.objects.all()
+    return render(request, 'blog_with_type.html', context)
